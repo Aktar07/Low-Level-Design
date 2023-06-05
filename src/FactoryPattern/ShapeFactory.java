f2203699aa8da6d26042fef90d45d2111541c1e1
Aktar Rajja Khan <aktarkhn07@gmail.com>
2023-06-05 15:39:44 +0530

    Factory Design Pattern

---
+++ src/FactoryPattern/ShapeFactory.java
@@ -0,0 +1,18 @@
+package FactoryPattern;
+
+public class ShapeFactory {
+
+    Shape getShape(String input){
+        switch (input){
+            case "CIRCLE":
+                return new Circle();
+            case "SQUARE":
+                return new Square();
+            case "RECTANGLE":
+                return new Rectangle();
+            default:
+                return null;
+
+        }
+    }
+}
